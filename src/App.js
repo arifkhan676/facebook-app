ff35ec4339b977d5e31f829bc12801281bdecfb5
arifkhan676 <arifulhasan5500@gmail.com>
2023-03-26 16:49:57 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+	Navbar,
+	NavItem,
+	NavbarToggler,
+	Collapse,
+	NavLink,
+	Nav,
+	NavbarBrand
+} from 'reactstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	// Collapse isOpen State
+	const [isOpen, setIsOpen] = React.useState(false);
+
+	return (
+		<div style={{
+			display: 'block', width: 550, padding: 30
+		}}>
+			<Navbar color="light" light expand="md">
+				<NavbarBrand href="/">Facebook</NavbarBrand>
+				<NavbarToggler onClick={() => { setIsOpen(!isOpen) }} />
+				<Collapse isOpen={isOpen} navbar>
+					<Nav className="mr-auto" navbar>
+						<NavItem>
+							<NavLink href="#">Home</NavLink>
+						</NavItem>
+						<NavItem>
+             <input className="head-search" type="search" placeholder="Search Facebook" aria-label="Search" />          
+						</NavItem>
+						<NavItem>
+							<NavLink href="#">Signup</NavLink>
+						</NavItem>
+					</Nav>
+				</Collapse>
+			</Navbar>
+		</div >
+	);
 }
 
 export default App;
