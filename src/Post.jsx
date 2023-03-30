3a58789ce657e6577a56ec7ca2c90876852dd038
arifkhan676 <2014751049@uits.edu.bd>
2023-03-30 23:44:41 +0600

    try to photo upload

--- src/Post.jsx
+++ src/Post.jsx
@@ -9,21 +9,38 @@ function Post(){
   
  //  const getTime = new Date().toLocaleTimeString();
   const [postStatus,setPost] = useState("");
-  const [userInput,setInput] = useState([]);
+  const [userInput,setInput] = useState([]); // here is an array for collected values
+  
+  /* var takePic = document.getElementById("profile-pic");
+   var fileInput = document.getElementById("myInputId");
+   fileInput.onchange=function (){
+   takePic.src = URL.createObjectURL(fileInput.files[0]);
+}*/
+
+  const [Picup,setupPic] = useState("");
+  const [Upload,setUpload] = useState([]);
 
   function handleChange(event){
-    const x = event.target.value;
-    setPost(x);
+    const x = event.target.value; //retrieve the value from input
+    setPost(x);  // and pass it to the value of postStatus
   }
 
   function handleClick(event){
-     setInput((preValue) =>{
-      return [...preValue,postStatus]
+     setInput((preValue) =>{ //pass the previous value to the userInput 
+      return [...preValue,postStatus] //used spred operator for collecting values from input and insert as an array index 
      });
-     setPost("");
+     setPost(""); 
   }
+
+  function changeUp(event){
+      const imgTag =  event.target.src;
+      setUpload(imgTag);
+  }
+
   function PhotoUp(){
-    
+    setupPic((...preValue)=>{
+      return [...preValue,]
+    })
   }
 
     return (
@@ -37,20 +54,20 @@ function Post(){
             className="head-search" 
             value={postStatus}
            /> 
-           <button onClick={handleClick} className="btn btn-secondary">Post</button>          
+           <button onChange={changeUp} onClick={handleClick} className="btn btn-secondary">Post</button>          
             </div>
             <div className="input-post-2">
               <DuoIcon className="DuoIcon" />
-              <PhotoSizeSelectActualIcon
-               className="ImgIcon"
-                onClick={PhotoUp}
+             <button onClick={PhotoUp} className="ImgIcon">
+             <PhotoSizeSelectActualIcon 
                 />
+               </button> 
               <EmojiEmotionsIcon  className="FeelIcon"/>
             </div>
-            {userInput.map((Status)=>
+            {userInput.map((Status)=> // mapping through the userInput(all of array indexes) and given each element in postArea div
             (
             <div className="postArea">
-             <h6>{Status}</h6>
+             <h6>{Status}</h6>  
             </div>)
 
            )}
